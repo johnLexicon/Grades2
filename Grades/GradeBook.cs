14e49d829aa9ffd69a614833999b71c48594c853
John Lundgren <john.paul.lundgren@gmail.com>
2018-11-27 14:16:19 +0100

    Added the Grades And GradeBook classes.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public static float MAX_GRADE = 100.0f;
+        public static float MIN_GRADE = 0.0f;
+
+        private List<float> grades = new List<float>();
+
+        public GradeStatistics ComputeGrades()
+        {
+            var stats = new GradeStatistics();
+            stats.MaxGrade = GetMaxGrade();
+            stats.MinGrade = GetMinGrade();
+            stats.AverageGrade = GetAverage();
+            return stats;
+        }
+
+        private float GetMaxGrade()
+        {
+            return grades.Max<float>();
+        }
+
+        private float GetMinGrade()
+        {
+            return grades.Min<float>();
+        }
+
+        private float GetAverage()
+        {
+            return grades.Average();
+        }
+    }
+}
---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,9 @@
+﻿namespace Grades
+{
+    class GradeStatistics
+    {
+        public float? MaxGrade { get; set; }
+        public float? MinGrade { get; set; }
+        public float? AverageGrade { get; set; }
+    }
+}
